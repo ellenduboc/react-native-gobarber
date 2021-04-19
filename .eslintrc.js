8a6c6425127b9654312838afe879c065ff38c516
Ellen Duboc <ellenduboc-fsa@hotmail.com>
2021-04-19 17:15:22 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+};
